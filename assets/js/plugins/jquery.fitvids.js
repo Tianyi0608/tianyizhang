2c6cb99408ece0771d5078f7d1e7b0e557a04d39
Tianyi0608 <50550260+Tianyi0608@users.noreply.github.com>
2023-08-24 17:28:53 +0800

    Update jquery.fitvids.js

--- assets/js/plugins/jquery.fitvids.js
+++ assets/js/plugins/jquery.fitvids.js
@@ -74,8 +74,7 @@
           var videoID = 'fitvid' + count;
           $this.attr('id', videoID);
         }
-        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', '20'+'%');
-        // .css('padding-top', (aspectRatio * 100)+'%');
+        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+'%');
         // $this.removeAttr('height').removeAttr('width');
       });
     });
